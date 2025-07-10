67a8ed6f71a2016040e893d0a1e358936918e898
Yermochenko Sergey <yermochenko@gmail.com>
2025-07-10 12:18:35 +0300

    Пример использование ретроспекции

--- src/main/java/Example4Pow.java
+++ src/main/java/Example4Pow.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Example4Pow {
 	/**
 	 * Возведение в степень
@@ -17,6 +20,16 @@ public class Example4Pow {
 		return result;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<?> x = Class.forName("java.util.Date");
+		Constructor<?> c1 = x.getConstructor();
+		Object obj1 = c1.newInstance();
+		System.out.println(obj1);
+		Constructor<?> c2 = x.getConstructor(long.class);
+		Object obj2 = c2.newInstance(0L);
+		System.out.println(obj2);
+		System.out.println(obj2.getClass() == obj2.getClass());
+		Object result = x.getMethod("after", x).invoke(obj1, obj2);
+		System.out.println(result);
 	}
 }
